3004f28b78692aa9d4427e937621f52eb5abaa1e
hao <960751327@qq.com>
2020-08-26 12:33:46 +0800

    小完善

--- 美团爬取商家信息/paquxinxi.py
+++ 美团爬取商家信息/paquxinxi.py
@@ -1,11 +1,7 @@
 import requests
 import re
 import json
-from urllib import parse
-import pandas as pd
-import time
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 headers = {
     "Origin": "https://bj.meituan.com",
     "Host": "apimobile.meituan.com",
@@ -39,33 +35,29 @@ def get_html(html):
     #联系电话
     phones = re.compile('"phone":"(.*?)",', re.S | re.I)
     phone = phones.findall(content)
-    # 人均消费
-    df = pd.DataFrame()
-    df['店名'] = title
-    df['地址'] = address
-    df['评分'] = avgscore
-    df['评价人数'] = comments
-    df['联系电话'] = phone
+    for i in range(len(title)):
+        try:
+            t = title[i]
+            a = address[i]
+            avg = avgscore[i]
+            c = comments[i]
+            p = phone[i]
+            print(t,a,avg,c,p)
+            dowload(t,a,avg,c,p)
+        except:
+            pass
 
-    df = df.drop_duplicates()
-    df = df.fillna('暂无数据')
-    cut = lambda x:'一般' if x<=3.5 else('不错' if x <=4.0 else("好" if x <=4.5 else "很好"))
-    df["评分类型"] = df['评分'].map(cut)
-    try:
-        df.to_csv("火锅信息.csv", mode="a+", header=None, index=None)
+def dowload(t,a,avg,c,p):
+    data = {
+        '店铺名称': t,
+        '店铺地址': a,
+        '店铺评分': avg,
+        '评价人数': c,
+        '电话': p
+    }
+    with open("美团信息.txt","a+",encoding="utf-8")as f:
+        f.write(json.dumps(data,ensure_ascii=False)+"\n")
         print("写入成功")
-    except:
-        print("当页数据写入失败")
-    time.sleep(1)
-    # #设置加载的字体名
-    # plt.rcParams['font.sans-serif'] = ['SimHei']
-    # #解放保存图像是负号"-"显示为方块的问题
-    # plt.rcParams['axes.unicode_minus'] = False
-    # fig,axes = plt.subplots(2,1,figsize=(12,12))
-    # sns.regplot(x= "人均消费",y = "评分",data = df,color='r',marker="+",ax = axes[0])
-    # sns.regplot(x="评价人数", y="评分", data=df, color='g', marker="*", ax=axes[1])
-
-
 if __name__ == '__main__':
     #在这个URL里面offse参数每次翻页增加32，limit参数是一次请求的数据量，q是搜索关键词poi/pcsearch/1？其中的1是北京城市的id编号。
     for i in range(0,33,32):
